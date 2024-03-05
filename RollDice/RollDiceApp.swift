e14dbe35d072a294a08ab3c45e15a580098bdfff
Leo Barnett <leo.e.barnett@icloud.com>
2024-03-05 16:26:50 +0100

    Initial Commit

---
+++ RollDice/RollDiceApp.swift
@@ -0,0 +1,17 @@
+//
+//  RollDiceApp.swift
+//  RollDice
+//
+//  Created by Leo  on 05.03.24.
+//
+
+import SwiftUI
+
+@main
+struct RollDiceApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
