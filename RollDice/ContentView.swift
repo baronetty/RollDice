54ae8436f02dc9acbb4d04567c2a04b5125baa2a
Leo Barnett <leo.e.barnett@icloud.com>
2024-03-05 17:01:09 +0100

    Version 1.0

--- RollDice/ContentView.swift
+++ RollDice/ContentView.swift
@@ -8,14 +8,51 @@
 import SwiftUI
 
 struct ContentView: View {
+    @State private var result: Int = 1
+    @State private var rolls: [Int] = []
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        ZStack {
+            Color(.black)
+            
+            VStack {
+                Spacer()
+                
+                Text("\(result)")
+                    .padding()
+                    .font(.largeTitle.bold())
+                    .foregroundStyle(.white)
+                Text("Your dice rolls so far:")
+                    .padding(.top, 50)
+                    .font(.title3)
+                    .foregroundStyle(.white)
+                Text("\(rolls.map(String.init).joined(separator: ", "))")
+                    .padding()
+                    .foregroundStyle(.gray)
+                
+                Spacer()
+                
+                Spacer()
+                
+                Button {
+                    rollDice()
+                } label: {
+                    Image(systemName: "cube")
+                    Text("Roll the dice")
+                }
+                .foregroundStyle(.purple)
+                .buttonStyle(.bordered)
+                
+                Spacer()
+            }
         }
-        .padding()
+        .ignoresSafeArea()
+    }
+    
+    
+    func rollDice() {
+        result = Int.random(in: 1...6)
+        rolls.append(result)
     }
 }
 
